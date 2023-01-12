31505cc5597ca739b4dec8178d284392c0c13800
Sabrina Ahamed <tazrian.zawad14@gmail.com>
2023-01-12 21:34:11 +0100

    nav bar done

--- react-protfolio/src/sections/navbar/Navbar.jsx
+++ react-protfolio/src/sections/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
+import Logo from '../../assets/logo.png'
 import './navbar.css'
+import data from './data'
+import {IoIosColorPalette} from 'react-icons/io'
 
 const Navbar = () => {
   return (
-   <navbar><h2>Navbar</h2></navbar>
+    <nav>
+      <div className="container nav_container">
+        <a href="index.html" className="nav_logo">
+         <img src={Logo} alt="Logo"/>
+        </a>
+       <ul className='nav_menu'>
+        {
+          data.map(item =>
+          <li key={item.id}>
+            <a href={item.link}>{item.title}
+            </a>
+          </li>)
+        }
+       </ul>
+       <button id='theme_icon'><IoIosColorPalette/></button>
+      </div>
+    </nav>
   )
 }
 
