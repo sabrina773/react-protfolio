498fe8ea35c51c3025ad89b3d786887e7c2325c0
Sabrina Ahamed <tazrian.zawad14@gmail.com>
2023-01-13 23:37:55 +0100

    About section done

--- react-protfolio/src/sections/about/About.jsx
+++ react-protfolio/src/sections/about/About.jsx
@@ -2,20 +2,30 @@ import AboutImage from '../../assets/about.jpeg'
 import  './about.css'
 import CV from '../../assets/Cv german.pdf'
 import {AiOutlineDownload} from 'react-icons/ai'
+import Card from '../../components/Card'
+import data from './data'
 
 const About = () => {
   return (
     <section id="about">
       <div className="container about_container">
-        <div className="about__left">
+        <div className="about_left">
           <div className="about_portrait">
-            <img src={AboutImage} alt="About Image"/>
+            <img src={AboutImage} alt="About Img"/>
           </div>
         </div>
-        <div className="about__right">
+        <div className="about_right">
           <h2>About me</h2>
           <div className="about_cards">
-
+             {
+              data.map(item =>(
+                <Card key={item.id} className="about_card">
+                  <span className="about_card-icon">{item.icon}</span>
+                  <h5>{item.title}</h5>
+                  <small>{item.desc}</small>
+                </Card>
+              ))
+             }
           </div>
           <p>I am Sabrina Ahamed. Innovative ,optimised solution seeker. Excited to be at the deployment face of my new career as a web developer. I would like to jump at any opportunity to continue growing my skill set in the web developing field at full stack web developer.</p>
           <a href={CV} download className='btn primary'>Download CV
---
+++ react-protfolio/src/sections/about/data.js
@@ -0,0 +1,11 @@
+import {MdWork} from 'react-icons/md'
+import {GrProjects} from 'react-icons/gr'
+import {MdSchool} from 'react-icons/md'
+
+const data =[
+  {id:'1', icon:<MdWork/>, title:'Experience', desc:'6 months working'},
+  {id:'2', icon:<GrProjects/>, title:'Projects', desc:'7 completed'},
+  {id:'3', icon:<MdSchool/>, title:'Education', desc:'Le Wagon Germany'}
+]
+
+export default data;
