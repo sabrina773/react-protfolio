c47e9aaf062f2fddcd6d1021341f349596bbb41b
Sabrina Ahamed <tazrian.zawad14@gmail.com>
2023-01-13 02:52:47 +0100

    Header section done

--- react-protfolio/src/sections/header/Header.jsx
+++ react-protfolio/src/sections/header/Header.jsx
@@ -1,8 +1,31 @@
+import HeaderImage from '../../assets/logo.png'
 import './header.css'
+import data from './data'
 
 const Header = () => {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header id="header">
+      <div className="container header_container">
+        <div className="header_profile">
+          <img src={HeaderImage} alt="Header Portait"/>
+        </div>
+        <h3>SABRINA AHAMED</h3>
+        <p>
+          You are click away building your dream website or webapp .Send me the details of your project for a modern , mobile responsive, highly performant website today!
+        </p>
+        <div className="header_cta">
+          <a href="#contact" className='btn primary'>Let's talk</a>
+          <a href="#protfolio" className='btn light'>My Work</a>
+        </div>
+        <div className="header_socials">
+          {
+           data.map(item =>
+            <a key={item.id} href={item.link} target="_blank" rel = "noopenner noreferrer">{item.icon}</a>)
+          }
+        </div>
+
+      </div>
+    </header>
   )
 }
 
---
+++ react-protfolio/src/sections/header/data.js
@@ -0,0 +1,9 @@
+import {AiFillLinkedin} from 'react-icons/ai';
+import {AiFillGithub} from 'react-icons/ai';
+
+const data = [
+ {id: '1', link:'https://www.linkedin.com/in/sabrina-ahamed-5a5b87bb/', icon:<AiFillLinkedin/>},
+ {id: '2', link:'https://github.com/sabrina773', icon:<AiFillGithub/>}
+]
+
+export default data
